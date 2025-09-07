e2372b680658280381e6fdcd56a06fa7b811cb95
Belgarion <dev.rorybolan@gmail.com>
2025-09-07 18:49:54 +0200

    changed package from app.belgarion to app.belgarion.UNO

---
+++ src/main/java/app/belgarion/UNO/Game.java
@@ -0,0 +1,89 @@
+package app.belgarion;
+
+import app.belgarion.AI.AI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import org.jetbrains.annotations.Nullable;
+
+
+public class Game {
+    private Pile pile;
+    private Deck deck;
+
+    public Game() {
+        this.pile = new Pile();
+        this.deck = new Deck();
+    }
+    public boolean check(Card card) {
+        return pile.isPlayable(card);
+
+    }
+    public static void printf(String s, Object... args) {
+        System.out.printf(s, args);
+    }
+
+    public static void print(String s) {
+        System.out.print(s);
+    }
+
+    public static void println(@Nullable String s) {
+        System.out.println(s != null ? s : "");
+    }
+
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        print("How many will be playing? ");
+        int players = scanner.nextInt();
+
+
+        ArrayList<ArrayList<Card>> cards = deck.Deal(players);
+        ArrayList<Card> player1_cards = cards.getFirst();
+        for (Card card : player1_cards) {
+            printf("Card: %s %s\n", card.cardColour, card.cardSymbol);
+
+        }
+        Player player = new Player(player1_cards);
+
+        List<ArrayList<Card>> botCards = cards.subList(1, cards.size());
+        ArrayList<AI> bots = new ArrayList<>();
+        for (ArrayList<Card> card_list : botCards) {
+            bots.add(new AI(card_list));
+        }
+        printf("The first card is a %s %s\n", pile.pile.peek().cardColour, pile.pile.peek().cardSymbol);
+        ArrayList<Card> playable = new ArrayList<>();
+        for (Card card : player.cards) {
+            if (pile.isPlayable(card)) {
+                playable.add(card);
+            }
+        }
+        StringBuilder finalstr = new StringBuilder();
+        for (Card card : playable) {
+            finalstr.append(card.cardColour).append(" ").append(card.cardSymbol).append("\n");
+        }
+        printf("The cards you can play are:\n %s\n", finalstr);
+        if (playable.isEmpty()) {
+            println("you need to draw a card");
+        }
+        println(!playable.isEmpty() ? "1: play a card\n2: draw a card" : "2: draw a card (no cards to play)");
+        int response = scanner.nextInt();
+        switch (response) {
+            case 1:
+                assert !playable.isEmpty();
+                for (int i = 0; i < playable.size(); i++) {
+                    printf("%d | %s %s", i, playable.getFirst().cardColour, playable.getFirst().cardSymbol);
+                }
+                println(null);
+                int card_choice = scanner.nextInt();
+                Card card_played = playable.remove(card_choice);
+                printf("Card %s %s played", card_played.cardColour, card_played.cardSymbol);
+                break;
+            case 2:
+                player1_cards.add(deck.draw());
+                printf("picked a %s %s", player1_cards.getLast().cardColour, player1_cards.getLast().cardSymbol);
+                break;
+            default:
+
+        }
+    }
+}
---
+++ src/main/java/app/belgarion/UNO/Player.java
@@ -0,0 +1,19 @@
+package app.belgarion;
+
+import java.util.ArrayList;
+
+public class Player {
+    public ArrayList<Card> cards;
+    public Player(ArrayList<Card> cards) {
+        this.cards = cards;
+    }
+    public ArrayList<Card> getPlayable(Card card_below) {
+        ArrayList<Card> playable = new ArrayList<>();
+        for (Card card : cards) {
+            if (Main.game.check(card)) {
+                playable.add(card);
+            }
+        }
+        return playable;
+    }
+}
